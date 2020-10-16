17c8d6edfa7c98375ee338ac4a0b84970ffbf13c
Advait Bhat <advaitmb@gmail.com>
2020-10-17 04:15:25 +0530

    added the model

---
+++ app/templates/base.sql
@@ -0,0 +1,11 @@
+CREATE TABLE user_data (
+    id SERIAL PRIMARY KEY,
+    baseline VARCHAR NOT NULL,
+    task1a VARCHAR NOT NULL, 
+    task1b VARCHAR NOT NULL,
+    task2a VARCHAR NOT NULL,
+    task2b VARCHAR NOT NULL,
+
+
+
+)
